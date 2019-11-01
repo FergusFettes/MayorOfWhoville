3bb0167f9ad89a6bc1c4e7b5a248a1e6d05cf6f8
Fergus <fergusfettes@gmail.com>
2019-11-01 20:44:15 +0000

    ready to test new client

--- websocket_simple_start.py
+++ websocket_simple_start.py
@@ -8,45 +8,43 @@ FORMAT = "%(levelname)s@%(name)s(%(asctime)s) -- \"%(message)s\""
 logging.basicConfig(level=logging.INFO, format=FORMAT)
 
 class Client:
+    def __init__(self, address):
+        self.uri = "ws://localhost:{}".format(address)
+        self.name = ''
 
-    async def print_message_angrily(self,address):
-        uri = "ws://localhost:{}".format(address)
-        async with websockets.connect(uri) as websocket:
-            name = await websocket.recv()
-            logging.info("I have a name! My name is {}".format(name))
-            while True:
-                await asyncio.sleep(ra.random() * 3)
-                message = await websocket.recv()
-                logging.info("{} says: Fuckin {}!".format(name, message))
+    async def initiate(self):
+        async with websockets.connect(self.uri) as websocket:
+            self.name = await websocket.recv()
+            logging.info("I have a name! My name is {}".format(self.name))
+            await self.choose_my_function()
 
-    async def print_message_happily(self,address):
-        uri = "ws://localhost:{}".format(address)
-        async with websockets.connect(uri) as websocket:
-            name = await websocket.recv()
-            logging.info("I have a name! My name is {}".format(name))
-            while True:
-                await asyncio.sleep(ra.random() * 3)
-                message = await websocket.recv()
-                logging.info("{} says: Love those {}!".format(name, message))
+    async def print_message_angrily(self):
+        while True:
+            await asyncio.sleep(ra.random() * 3)
+            message = await websocket.recv()
+            logging.info("{} says: Fuckin {}!".format(name, message))
 
-    async def send_message_back(self,address):
-        uri = "ws://localhost:{}".format(address)
-        async with websockets.connect(uri) as websocket:
-            name = await websocket.recv()
-            logging.info("I have a name! My name is {}".format(name))
-            while True:
-                await asyncio.sleep(ra.random() * 3)
-                await websocket.send("hello from {} servi!".format(name))
+    async def print_message_happily(self):
+        while True:
+            await asyncio.sleep(ra.random() * 3)
+            message = await websocket.recv()
+            logging.info("{} says: Love those {}!".format(name, message))
 
+    async def send_message_back(self):
+        while True:
+            await asyncio.sleep(ra.random() * 3)
+            await websocket.send("hello from {} servi!".format(name))
 
-    async def server_and_clients(self,address):
+    async def choose_my_function(self):
         tasks = []
-        tasks.append(print_message_angrily(address))
-        tasks.append(print_message_happily(address))
-        tasks.append(send_message_back(address))
-        await asyncio.gather(*tasks)
+        tasks.append(print_message_angrily)
+        tasks.append(print_message_happily)
+        tasks.append(send_message_back)
+        choice = ra.randint(0, len(tasks) - 1)
+        await tasks[choice]()
 
 if __name__=="__main__":
     address = 8002
-    asyncio.get_event_loop().run_until_complete(server_and_clients(address))
+    c = Client(address)
+    asyncio.get_event_loop().run_until_complete(c.initiate())
     asyncio.get_event_loop().run_forever()
