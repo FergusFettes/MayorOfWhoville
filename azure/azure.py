78cd2c5ff4196237af8d8c49c0d6a05d1cb379bc
Fergus <fergusfettes@gmail.com>
2019-11-03 12:57:18 +0000

    i tihnk the only problem i have been having this whole time has been with codecs and data formats to be honest

--- azure/azure.py
+++ azure/azure.py
@@ -13,79 +13,30 @@ FILE = PATH + "/output"
 WAV_PARAMS = wave._wave_params(1, 1, 8000, 128000, 'NONE', 'not compressed')
 
 class StreamTranscriber:
-    CHUNK = 1024
-    CLIENT_HANDSHAKE = "How dyou do?"
-    CANCEL_TRANSMISSION = "Please shutdown the connection"
-    AUDIOSTREAM = "Stream coming through double quick"
-
-    def __init__(self):
-        self.CONNECTED = {}
-        self.CHILDREN = {}
-
     def setup_server(self, address):
         return websockets.serve(self.async_init, "localhost", address)
 
     async def async_init(self, websocket, path):
-        handshake = await websocket.recv()
-        if handshake == self.CLIENT_HANDSHAKE:
-            await self.main_server_activities(websocket)
-        elif handshake == self.AUDIOSTREAM:
-            parent = await websocket.recv()
-            logging.info(parent)
-            if parent in self.CONNECTED.keys():
-                self.CHILDREN[self.CONNECTED[parent]] = websocket
-                logging.info("Accepting transmission")
-                await self.receive_stream(websocket)
+        logging.warning("Prepairing to receive stream")
+        await self.receive_stream(websocket)
+        logging.warning("Stream received, disconnecting")
 
     async def receive_stream(self, websocket):
-        name = "{}_{}.wav".format(FILE, str(hash(websocket)))
+        # name = "{}_{}.wav".format(FILE, str(hash(websocket)))
+        name = FILE + ".wav"
+        params = await websocket.recv()
+        params = params.split(',')
         with wave.open(name, 'wb') as fi:
-            fi.setparams(WAV_PARAMS)
+            fi.setnchannels(int(params[0]))
+            fi.setsampwidth(int(params[1]))
+            fi.setframerate(int(params[2]))
             while True:
                 data = await websocket.recv()
+                logging.info("Bytes received")
                 if not data:
+                    logging.info("Empty packet received, closing")
                     break
                 fi.writeframes(data)
-
-    async def main_server_activities(self, websocket):
-        await self.identify(websocket)
-        try:
-            await self.main_server_functions(websocket)
-        finally:
-            self.CONNECTED.pop(str(hash(websocket)))
-
-    async def identify(self, websocket):
-        socketid = hash(websocket)
-        self.CONNECTED[str(socketid)] = websocket
-        logging.info(self.CONNECTED)
-        await websocket.send(str(socketid))
-
-    async def main_server_functions(self, websocket):
-        activities = []
-        activities.append(self.receive_message(websocket))
-        activities.append(self.send_message(websocket))
-        done, pending = await asyncio.wait(activities,
-                return_when=asyncio.FIRST_COMPLETED)
-        for activity in pending:
-            activity.cancel()
-        # The other main setup, when you want them all to happen simultaneously and dont care when then return
-        # await asyncio.gather(*activities)
-
-    async def receive_message(self, websocket):
-        while True:
-            message = await websocket.recv()
-            logging.info("Server received message: {}".format(message))
-            if message == self.CANCEL_TRANSMISSION:
-                for websocket in self.CHILDREN[websocket]:
-                    websocket.close()
-            await asyncio.sleep(10)
-
-    async def send_message(self, websocket):
-        while True:
-            whatever = "whatever"
-            await websocket.send(whatever)
-            logging.info("Server sent messge")
-            await asyncio.sleep(10)
 
 if __name__=="__main__":
     address = 8003
--- azure/host_streamer.py
+++ azure/host_streamer.py
@@ -1,7 +1,9 @@
 import argparse
+import time
 import os
 import wave
 import pyaudio
+import pysoundio
 import websockets
 import asyncio
 import logging
@@ -18,126 +20,85 @@ WAV_PARAMS = wave._wave_params(1, 1, 8000, 0, 'NONE', 'not compressed')
 
 
 class PyAudioStreamer:
-    def __init__(self, audio_buffer):
+    def __init__(self):
         self.pyaudio_object = pyaudio.PyAudio()
-        self.audio_buffer = audio_buffer
+        self.params = "1,2,44100"
         self.stream = self.pyaudio_object.open(
-            format=self.pyaudio_object.get_format_from_width(1),
             channels=1,
-            rate=8000,
+            format=pyaudio.paInt16,
+            rate=44100,
             input=True,
-            start=False,
-            stream_callback=self.callback,
+            frames_per_buffer=1024,
         )
-
-    def callback(self, in_data, frame_count, time_info, status):
-        while True:
-            self.audio_buffer.put(in_data)
-
     def close(self):
         self.stream.close()
         self.pyaudio_object.terminate()
 
-    def start(self):
-        self.stream.start_stream()
+class PySoundIoStreamer:
+    def __init__(self):
+        self.pysoundio_object = pysoundio.PySoundIo(backend=None)
+        self.params = "1,1,44100"
+        self.buffer = queue.Queue(maxsize=CHUNK * 10)
+        class Stream: pass
+        self.stream = Stream()
+        self.stream.read = self.buffer.get()
+        self.pysoundio_object.start_input_stream(
+            device_id=None,
+            channels=1,
+            sample_rate=44100,
+            block_size=1024,
+            dtype=pysoundio.SoundIoFormatU8,
+            read_callback=self.callback
+        )
 
-class FileStreamer:
-    def __init__(self, audio_buffer):
-        self.audio_buffer = audio_buffer
-        self.wf = wave.open(FILE)
-        self.thread = None
-
-    def start(self):
-        while True:
-            while self.audio_buffer.full():
-                time.sleep(0.1)
-            logging.info("Writing bytes from file to buffer")
-            data = self.wf.readframes(CHUNK)
-            if not data:
-                break
-            self.audio_buffer.put(data)
+    def callback(self, data, length):
+        self.audio_buffer.put(data)
 
     def close(self):
-        self.thread.join()
+        self.pysoundio_object.close()
+
+class FileStreamer:
+    def __init__(self):
+        self.wf = wave.open(FILE)
+        self.params = "1,1,8000"
+        class Stream: pass
+        self.stream = Stream()
+        self.stream.read = self.wf.readframes
+
+    def close(self):
         self.wf.close()
 
 class Streamer:
     TIMEOUT = 4
-    CHUNK = 1024
     CLIENT_HANDSHAKE = "How dyou do?"
     CANCEL_TRANSMISSION = "Please shutdown the connection"
     AUDIOSTREAM = "Stream coming through double quick"
     def __init__(self, address, streamer):
         self.uri = "ws://localhost:{}".format(address)
         self.name = 0
-        self.buf_max_size = self.CHUNK * 10
-        self.audio_buffer = queue.Queue(maxsize=self.buf_max_size)
+        self.buf_max_size = CHUNK * 10
         if streamer == "file":
-            self.streamer = FileStreamer(self.audio_buffer)
+            self.streamer = FileStreamer()
         elif streamer == "pyaudio":
-            self.streamer = PyAudioStreamer(self.audio_buffer)
-        # elif streamer == "pysoundio":
-        #     self.streamer = PySoundIoStreamer(self.audio_buffer)
+            self.streamer = PyAudioStreamer()
+        elif streamer == "pysoundio":
+            self.streamer = PySoundIoStreamer()
 
     async def register_with_server(self):
         async with websockets.connect(self.uri) as websocket:
-            await websocket.send(self.CLIENT_HANDSHAKE)
-            self.name = await websocket.recv()
-            stream = []
-            stream.append(self.send_stream())
-            stream.append(self.receive_message(websocket))
-            stream.append(self.send_message(websocket))
-            await asyncio.gather(*stream)
-
-    async def send_stream(self):
-        async with websockets.connect(self.uri) as websocket:
-            logging.info("Trying to send audiostream")
-            await websocket.send(self.AUDIOSTREAM)
-            await websocket.send(self.name)
+            logging.warning("Starting stream transmission")
             await self.stream_with_timeout(websocket)
             self.streamer.close()
+            logging.warning("Stream transmission ended successfully")
 
     async def stream_with_timeout(self, websocket):
-        stream_tasks = []
-        if type(self.streamer) is FileStreamer:
-            stream_tasks.append(self.streamer.start())
-        else:
-            self.streamer.start()
-        stream_tasks.append(self.forward_stream(websocket))
-        stream_tasks.append(self.timeout())
-        done, pending = await asyncio.wait(stream_tasks,
-                return_when=asyncio.FIRST_COMPLETED)
-        for task in pending:
-            task.cancel()
-        logging.info("Stream timed out")
-
-    async def timeout(self):
-        then = time.time()
-        logging.info("Waiting for timeout")
-        await asyncio.sleep(self.TIMEOUT)
-        diff = time.time() - then
-        logging.info("Timed out after {} seconds, returning".format(diff))
-
-    async def forward_stream(self, websocket):
+        await websocket.send(self.streamer.params)
         while True:
             logging.info("Sending bytes")
-            data = self.audio_buffer.get()
+            data = self.streamer.stream.read(CHUNK)
             if not data:
                 break
             await websocket.send(data)
-
-    async def receive_message(self, websocket):
-        while True:
-            message = await websocket.recv()
-            logging.info("Client recieved message: {}".format(message))
-            await asyncio.sleep(10)
-
-    async def send_message(self, websocket):
-        while True:
-            await websocket.send("Message from client")
-            logging.info("Client sent message")
-            await asyncio.sleep(10)
-
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description="Streams stuff to websocket")
